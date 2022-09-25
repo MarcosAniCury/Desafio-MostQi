8ade517e189a6ded360d9e0d40aecf8994390d91
MarcosAniCury <curymarcos@live.com>
2022-09-25 19:09:34 -0300

    Modify all project struct

---
+++ MostQuotation/ClientApp/src/Services/API/settings.js
@@ -0,0 +1,18 @@
+export const applicationJson = 'application/json';
+
+export const URL = {
+    base: 'https://localhost:7111/api/',
+    user: {
+        signup: 'user/signup',
+        signin: 'user/signin',
+        getById: 'user',
+        forgetPassword: 'user/forget-password',
+        recoverPassword: 'user/recover-password',
+    }, 
+    client: {
+        getAll: 'client/get',
+        create: 'client/create',
+        getByNameLike: 'client/get-by-name',
+        getByNameLikeAndCollaboratorLike: 'client/get-by-name-and-collaborator'
+    }
+}
